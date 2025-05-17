113528481268358ff1e03f77da092dbdcd5ea799
Axel Galicia <axelgalicia@gmail.com>
2025-05-17 12:27:40 -0400

    Initial changes 2025

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
-function reloadOrientation() {
-    window.addEventListener("orientationchange", function () {
-      location.reload();
-    }, false);
+const reloadOrientation = () => {
+  window.addEventListener("orientationchange", () => {
+    location.reload();
+  }, false);
+}
+
+const getYearsOfExperience = () => {
+  const startDate = new Date("2010-06-01");
+  const today = new Date();
+  const yearsOfExperience = today.getFullYear() - startDate.getFullYear();
+  return yearsOfExperience;
 }
